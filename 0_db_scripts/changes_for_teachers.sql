98274e30e3d3bbcbe2c5c1428e6841d2833b2765
Dmitry Stavinsky <diman.stavinskij2011@ya.ru>
2019-11-17 00:20:22 +0400

    Delete trash files

--- 0_db_scripts/changes_for_teachers.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `teachers` DROP `phone_number`;
-
-ALTER TABLE `teachers` 
-	CHANGE `academic_degree_id` 
-		`academic_degree_id` INT(10) UNSIGNED NULL, 
-	CHANGE `academic_rank_id` 
-		`academic_rank_id` INT(10) UNSIGNED NULL;
-		
-ALTER TABLE `teachers` DROP INDEX email_UNIQUE;
