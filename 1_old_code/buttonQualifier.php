dfa73b8bdc576efe12073eb412d517572fdecd43
Zakycka163 <diman.stavinskij2011@ya.ru>
2019-11-16 23:27:49 +0300

    Delete buttonQualifier.php

--- 1_old_code/buttonQualifier.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    require_once ($_SERVER['DOCUMENT_ROOT']."../blocks/base.php");
-            
-    if(!empty($_SESSION["id"])){
-        require_once ($_SERVER['DOCUMENT_ROOT']."../blocks/buttons/exit.php");
-    } else {
-		require_once ($_SERVER['DOCUMENT_ROOT']."../blocks/buttons/sign_in.php");
-    }
-?>
