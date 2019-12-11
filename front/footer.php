3f62ca73b6b4159743f65d2c174774a590fd3597
Zakycka163 <diman.stavinskij2011@ya.ru>
2019-12-11 20:51:43 +0400

    Footer was fixed

--- front/footer.php
+++ front/footer.php
@@ -3,7 +3,7 @@
 		<div class="p-1 bg-white font-weight-bold text-primary">
 			<?php echo 'Тольятти 2019'. "\n";?>
 			<a href="http://www.tolgas.ru" title="Сайт ПВГУС">
-				<img src="/img/pvgus_ico2.png" height="45px" alt="ПВГУС" title="Сайт ПВГУС">
+				<img src="../front/img/pvgus_ico2.png" height="45px" alt="ПВГУС" title="Сайт ПВГУС">
 			</a>
 		</div>
 	</center>
