f58a9edfa41d817b5b4e55c366c8a4fc06c04712
Zakycka163 <diman.stavinskij2011@ya.ru>
2019-12-15 01:10:26 +0400

    extra row was deleted

--- back/security/validator.php
+++ back/security/validator.php
@@ -2,8 +2,7 @@
     if(empty($_SESSION["id"])){
 		if( $_SERVER['REQUEST_URI']<>"/index.php"
 		and $_SERVER['REQUEST_URI']<>"/pages/sign_in.php"
-		and $_SERVER['REQUEST_URI']<>"/pages/about.php")
-		{
+		and $_SERVER['REQUEST_URI']<>"/pages/about.php"){
 			header("Location: ../index.php");
 		} 
     }
