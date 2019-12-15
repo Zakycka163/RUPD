5c96a69fd76ff34a8bb58736f0897da084c50bd9
Zakycka163 <diman.stavinskij2011@ya.ru>
2019-12-15 22:28:33 +0400

    Extra spaces was removed

--- index.php
+++ index.php
@@ -4,18 +4,13 @@
   <head>
     <meta charset="utf-8">
     <title>Разработка</title>
-
     <?php require_once ($_SERVER['DOCUMENT_ROOT']."/front/links.php"); ?>
-
   </head>
   <body>
     <?php require_once ($_SERVER['DOCUMENT_ROOT']."/back/mandatoryBlock.php"); ?>
-
     <center>
       <div class="p-3 bg-primary font-weight-bold text-white"><h3>Главная</h3></div>
     </center>
-
     <?php require_once ($_SERVER['DOCUMENT_ROOT']."/back/detectors/tasks.php"); ?>
-
   </body>
 </html>
