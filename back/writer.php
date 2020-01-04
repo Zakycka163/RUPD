d96996b55cf33977391f3bad64e5b085f13853aa
Dmitry <diman.stavinskij2011@ya.ru>
2020-01-04 22:45:26 +0400

    Saver -> Writer. Reading a 'teachers' file

---
+++ back/writer.php
@@ -0,0 +1,51 @@
+<?php 
+	function read($uploadfile, $maxColumn) {
+		require_once ($_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php");
+
+		$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+		$reader->setReadDataOnly(TRUE);
+		$spreadsheet = $reader->load($uploadfile);
+
+		$worksheet = $spreadsheet->getActiveSheet();
+		// Get the highest row and column numbers referenced in the worksheet
+		$highestRow = $worksheet->getHighestRow(); // e.g. 10
+		$highestColumn = $maxColumn; // e.g 'F'
+		$highestColumn++;
+
+        echo "INSERT INTO `teachers` (`second_name`, `first_name`, `middle_name`, `email`, `academic_rank_id`, `academic_degree_id`) VALUES ";
+		for ($row = 2; $row <= $highestRow; ++$row) {
+			echo ( ($row == 2) ? "(" : ", (" );
+
+			for ($col = 'A'; $col != $highestColumn; ++$col) {
+				echo ( ($col == 'A') ? "'" : ", '" );
+					
+				$value = $worksheet->getCell($col . $row)->getValue();
+
+				if ($col == 'E') {
+					connect();
+					global $link;
+					$result = mysqli_query($link, "SELECT academic_rank_id FROM academic_ranks WHERE UPPER(short_name) = UPPER('".$value."')");
+					while($p1 = mysqli_fetch_array($result)){
+						echo $p1;
+					}
+					close();
+				} elseif ($col == 'F') {
+					connect();
+					global $link;
+					$result = mysqli_query($link, "SELECT academic_degree_id FROM academic_degrees WHERE UPPER(short_name) =UPPER('".$value."')");
+					while($p1 = mysqli_fetch_array($result)){
+						echo $p1;
+					}
+					close();
+				} else {
+					echo $value;
+				}
+
+				echo "'" . PHP_EOL;
+			};
+			echo ')' . PHP_EOL;
+		};
+		echo ";" . PHP_EOL;
+	};
+	
+?> 
