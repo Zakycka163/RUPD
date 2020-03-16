3a9617218485856f0bda1683b3c78e8352d173ad
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-03-16 18:19:41 +0300

    View users_presenter is added

---
+++ database/view.sql
@@ -0,0 +1,13 @@
+CREATE
+/* [DEFINER = { user | CURRENT_USER }]*/
+VIEW users_presenter
+AS(
+	SELECT    acc.account_id 
+			, acc.login
+			, acc.grant_id
+			, teach.second_name
+			, teach.first_name
+			, teach.middle_name
+	FROM  `accounts` acc
+		, `teachers` teach 
+	WHERE acc.teacher_id = teach.teacher_id)
