debf92a28d1b014213fdc2a76f0c8f2fb8fef552
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-03-17 10:39:02 +0300

    Refactoring user page

--- back/body/user.php
+++ back/body/user.php
@@ -15,7 +15,7 @@
 			$grant_to_edit_account_name = "";
 		} else {
 			$admin = "";
-			$grant_to_edit_account_name = "disabled";
+			$grant_to_edit_account_name = "hidden";
 		}		
 	};
 	close();
@@ -29,11 +29,11 @@
 		<input class="btn btn-success btn-mg" type="button" value="Сохранить">
 	</div>
 </div>
-<table class="table table-borderless" style="width: 65rem">
+<table class="table table-borderless" style="width: 50rem">
 	<tr>
-		<td class="align-middle" style="width: 15rem">Фамилия</td>
+		<td class="align-middle" style="width: 7rem">Фамилия</td>
 		<td>
-			<div class="input-group" style="right: 15px;">
+			<div class="input-group input-group-sm" style="right: 15px;">
 				<input class="form-control" type="text" id="second_name" value="<?php echo $second_name; ?>">
 			</div>
 		</td>
@@ -43,7 +43,7 @@
 	<tr>
 		<td class="align-middle">Имя</td>
 		<td>
-			<div class="input-group" style="right: 15px;">
+			<div class="input-group input-group-sm" style="right: 15px;">
 				<input class="form-control" type="text" id="first_name" value="<?php echo $first_name; ?>">
 			</div>
 		</td>
@@ -53,7 +53,7 @@
 	<tr>
 		<td class="align-middle">Отчество</td>
 		<td>
-			<div class="input-group" style="right: 15px;">
+			<div class="input-group input-group-sm" style="right: 15px;">
 				<input class="form-control" type="text" id="middle_name" value="<?php echo $middle_name; ?>">
 			</div>
 		</td>
@@ -66,43 +66,29 @@
 	<tr>
 		<td class="align-middle">Логин</td>
 		<td>
-			<div class="input-group" style="right: 15px;">
-				<input class="form-control" type="text" id="login" value="<?php echo $login; ?>" <?php echo $grant_to_edit_account_name; ?> >
+			<div class="input-group input-group-sm" style="right: 15px;">
+				<input class="form-control" type="text" id="login" value="<?php echo $login; ?>" disabled>
+				<div class="input-group-append" <?php echo $grant_to_edit_account_name; ?> >
+					<button class="btn btn-outline-primary" type="button">Изменить</button>
+				</div>
 			</div>
 		</td>
-		<td class="align-middle" style="width: 13rem">Права администратора</td>
-		<td style="width: 12rem">
-			<div class="custom-control custom-switch" style="top: 10px">
+		<td class="align-middle" style="width: 7rem">Администратор</td>
+		<td style="width: 7rem">
+			<div class="custom-control custom-switch" style="top: 5px">
 				<input type="checkbox" class="custom-control-input" <?php echo $admin; ?> id="switch">
 				<label class="custom-control-label"></label>
 			</div>
 		</td>
 	</tr>
 	<tr>
-		<td class="align-middle">Старый пароль</td>
+		<td class="align-middle">Пароль</td>
 		<td>
-			<div class="input-group" style="right: 15px;">
-				<input class="form-control" type="text" id="pass">
-			</div>
-		</td>
-		<td class="align-middle"></td>
-		<td></td>
-	</tr>
-	<tr>
-		<td class="align-middle">Новый пароль</td>
-		<td>
-			<div class="input-group" style="right: 15px;">
-				<input class="form-control" type="text" id="new_pass">
-			</div>
-		</td>
-		<td class="align-middle"></td>
-		<td></td>
-	</tr>
-	<tr>
-		<td class="align-middle">Повторите пароль</td>
-		<td>
-			<div class="input-group" style="right: 15px;">
-				<input class="form-control" type="text" id="repl_pass">
+			<div class="input-group input-group-sm" style="right: 15px;">
+				<input class="form-control" type="text" id="pass" value="************" disabled>
+				<div class="input-group-append" <?php echo $grant_to_edit_account_name; ?> >
+					<button class="btn btn-outline-primary" type="button">Изменить</button>
+				</div>
 			</div>
 		</td>
 		<td class="align-middle"></td>
