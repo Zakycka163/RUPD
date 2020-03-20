c98f94b7df81b702ff7b24ea04a317259520f56b
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-03-20 11:48:00 +0300

    Create loader_form.php

---
+++ front/forms/loader_form.php
@@ -0,0 +1,14 @@
+<div class="modal fade" id="loader_form" tabindex="-1" role="dialog">
+	<div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h6 class="modal-title">Подождите, идет обработка</h6>
+			</div>
+			<div class="modal-body">
+				<div class="progress">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
