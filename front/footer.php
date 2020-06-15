b03e91823428b947390b51a74822f4fa2c415ed3
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-06-15 15:52:44 +0300

    Link will open on new tab

--- front/footer.php
+++ front/footer.php
@@ -2,7 +2,7 @@
     <center>
         <div class="p-1 bg-white font-weight-bold text-primary">
             <?php echo 'Тольятти 2020'. "\n";?>
-            <a href="http://www.tolgas.ru" title="Сайт ПВГУС">
+            <a href="http://www.tolgas.ru" target="_blank" title="Сайт ПВГУС">
                 <img src="/front/img/pvgus_ico2.png" height="45px" alt="ПВГУС" title="Сайт ПВГУС">
             </a>
         </div>
