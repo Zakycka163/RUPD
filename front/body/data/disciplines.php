dc512c8638a79ac0e922532b9629fffc130091a8
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-07-21 13:06:10 +0300

    Add discipline page

--- front/body/data/disciplines.php
+++ front/body/data/disciplines.php
@@ -1,57 +1,47 @@
-<?php
-	if (isset($_GET["id"])){
-		require_once ($_SERVER['DOCUMENT_ROOT']."/front/body/single_obj/discipline.php");
-		exit();
-	}
-?>
 <div class="px-4 py-2 bg-primary font-weight-bold text-white container-fluid">
-	<div class="row">
-		<a class="btn btn-warning btn-sm back" href="/pages/data.php" style="height: 35px; width: 5rem; margin-left: 1rem" title="Назад" data-toggle="tooltip" data-placement="right">&#8592; Назад</a>
-		<div class="h4" id="page_title" style="margin-left: 35%">Дисциплины</div>
-	</div>
+    <div class="row">
+        <a class="btn btn-warning back" href="/pages/data.php"
+            style="margin-left: 1rem" title="Назад" data-toggle="tooltip"
+            data-placement="right">&#8592; Назад</a>
+        <div class="h4" id="page_title" style="margin-left: 35%">Дисциплины</div>
+    </div>
 </div>
 <div class="px-4 py-3 bg-light">
-	<div class="alert alert-secondary col" style="height: 55px">
-		<input class="btn btn-success btn-sm" type="button" id="create_discipline" value="Новая дисциплина">
-	</div>
-	<table class="table table-bordered table-striped table-sm">
-		<thead>
-			<tr>
-				<th scope="col" style="width: 2rem">№</th>
-				<th scope="col">Название дисциплины</th>
-				<th scope="col">Кафедра</th>
-				<th scope="col">Индекс</th>
-				<th scope="col">Модуль</th>
-				<th scope="col">Образовательная часть</th>
-				<th scope="col">Нагрузка, ч</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
+    <div class="alert alert-secondary col" style="height: 55px">
+        <input class="btn btn-success btn-sm" type="button" id="create_discipline" value="Новая дисциплина">
+    </div>
+    <table class="table table-bordered table-striped table-sm">
+        <thead>
+            <tr>
+                <th scope="col" style="width: 2rem">№</th>
+                <th scope="col">Название дисциплины</th>
+                <th scope="col">Кафедра</th>
+                <th scope="col">Индекс</th>
+                <th scope="col">Модуль</th>
+                <th scope="col">Образовательная часть</th>
+                <th scope="col">Нагрузка, ч</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
 				connect();
 				global $link;
 				$sql = "SELECT `value` FROM `constants` WHERE `key` = 'limitObj'";
 				$result = mysqli_query($link, $sql);
 				$limit = mysqli_fetch_array($result);
 				$counter = 0;
-				$sql_count = "SELECT count(*) FROM disciplines";
+				$sql_count = "SELECT count(*) FROM view_disciplines";
 				$sql_count_result = mysqli_query($link, $sql_count);
 				$count_obj = mysqli_fetch_array($sql_count_result);
-				$sql = "SELECT    kaf.pulpit_id
-								, kaf.name
-								, dis.discipline_id
-								, dis.name
-								, dis.index_info
-								, module.name
-								, part.name
-								, dis.time
-						FROM  `disciplines` dis
-							, `pulpits` kaf
-							, `modules` module
-							, `parts` part
-						WHERE 	dis.pulpit_id = kaf.pulpit_id
-							and dis.module_id = module.module_id
-							and dis.part_id = part.part_id
+				$sql = "SELECT    pulpit_id
+								, kaf
+								, discipline_id
+								, `name`
+								, index_info
+								, module
+								, part
+								, `time`
+						FROM  view_disciplines
 						LIMIT ".$limit[0]."";
 				$result = mysqli_query($link, $sql);
 				while($row = mysqli_fetch_array($result)){
@@ -67,74 +57,77 @@
 				};
 				close();
 			?>
-		</tbody>
-	</table>
-	<nav>
-		<ul class="pagination pagination-sm">
-			<?php if ($count_obj < $limit){
+        </tbody>
+    </table>
+    <nav>
+        <ul class="pagination pagination-sm">
+            <?php if ($count_obj < $limit){
 				
 			} else {
 			?>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">Предыдущая</a>
-				</li>
-				<li class="page-item active">
-					<a class="page-link" href="#">1</a>
-				</li>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">2</a>
-				</li>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">3</a>
-				</li>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">Следующая</a>
-				</li>
-			<?php } ?>
-		</ul>
-	</nav>	
-</div>	
+            <li class="page-item disabled">
+                <a class="page-link" href="#">Предыдущая</a>
+            </li>
+            <li class="page-item active">
+                <a class="page-link" href="#">1</a>
+            </li>
+            <li class="page-item disabled">
+                <a class="page-link" href="#">2</a>
+            </li>
+            <li class="page-item disabled">
+                <a class="page-link" href="#">3</a>
+            </li>
+            <li class="page-item disabled">
+                <a class="page-link" href="#">Следующая</a>
+            </li>
+            <?php } ?>
+        </ul>
+    </nav>
+</div>
 <?php 
 	require_once ($_SERVER['DOCUMENT_ROOT']."/front/forms/pulpit.php");
-?> 
+	require_once ($_SERVER['DOCUMENT_ROOT']."/front/forms/discipline.php");
+?>
 <script>
-	if ($_GET('kafid')) {
-		$('#delete_pulpit').prop('hidden', true);
-		$('#add_parent_inst_name').prop('hidden', true);
-		$('#save_pulpit').prop('hidden', true);
-		$('#pul_name').prop('readonly', true);
-		$('#pul_description').prop('readonly', true);
-		
-		$('#pulpit_form').modal('show');
-		$('#pulpit_form_title').text('Кафедра:');
-		let pul_id = $_GET('kafid');
-		$.post(
-		 	"/back/data/db_pulpits.php", 
-			{functionname: 'get_pulpit', id: pul_id}, 
-			function(info){
-				var pul = $.parseJSON(info);
-				$('#pulpit_form_title').after(pul.name);
-				$('#inst_val option:selected').prop('selected', false);
-				$('#inst_val').val(pul.institute_id).prop('selected', true);
-				$('#pul_name').val(pul.name);
-				$('#pul_description').text(pul.description);
-				
-				$("#add_parent_inst_name_form").find("#inst_val option:selected").text()
-				var inst_text = $("#inst_val :selected").text();
-				$("#parent_inst_name").val(inst_text);
-				$("#parent_inst_name").prop('title',inst_text);
-				$("#add_parent_inst_name").removeClass('btn-outline-success');
-				$("#add_parent_inst_name").addClass('btn-outline-primary');
-				$("#add_parent_inst_name").text('Изменить');
-			}
-		);
-	}
+$(".close_form").click(function() {
+    location.href = 'data.php?page=disciplines';
+});
+$("#create_discipline").click(function() {
+    $('#discipline_form').modal('show');;
+});
+create_discipline
 
-	$(".close_form").click(function(){
-		location.href='data.php?page=disciplines';
-	});
+if ($_GET('kafid')) {
+    $('#delete_pulpit').prop('hidden', true);
+    $('#add_parent_inst_name').prop('hidden', true);
+    $('#save_pulpit').prop('hidden', true);
+    $('#pul_name').prop('readonly', true);
+    $('#pul_description').prop('readonly', true);
 
-	$(".close").click(function(){
-		location.href='data.php?page=disciplines';
-	});
+    $('#pulpit_form').modal('show');
+    $('#pulpit_form_title').text('Кафедра:');
+    let pul_id = $_GET('kafid');
+    $.post(
+        "/back/data/db_pulpits.php", {
+            functionname: 'get_pulpit',
+            id: pul_id
+        },
+        function(info) {
+            var pul = $.parseJSON(info);
+            $('#pulpit_form_title').after(pul.name);
+            $('#inst_val option:selected').prop('selected', false);
+            $('#inst_val').val(pul.institute_id).prop('selected', true);
+            $('#pul_name').val(pul.name);
+            $('#pul_description').text(pul.description);
+
+            $("#add_parent_inst_name_form").find("#inst_val option:selected").text()
+            var inst_text = $("#inst_val :selected").text();
+            $("#parent_inst_name").val(inst_text);
+            $("#parent_inst_name").prop('title', inst_text);
+            $("#add_parent_inst_name").removeClass('btn-outline-success');
+            $("#add_parent_inst_name").addClass('btn-outline-primary');
+            $("#add_parent_inst_name").text('Изменить');
+        }
+    );
+}
 </script>
---
+++ front/forms/discipline.php
@@ -0,0 +1,266 @@
+<div class="modal fade" id="discipline_form" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div>
+                    <h5 class="modal-title" id="discipline_form_title">Создание дисциплины</h5>
+                </div>
+                <button type="button" class="close close_form" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-borderless table-sm">
+                    <tr>
+                        <td class="align-middle">Кафедра<span style="color: red">*</span></td>
+                        <td width="70%">
+                            <div class="input-group input-group-sm" id="parent_kaf" data-toggle="popover"
+                                data-placement="right" data-content="Нужно выбрать!">
+                                <input type="text" id="parent_kaf_name" class="form-control form-control-sm"
+                                    value="Отсутствует" data-toggle="tooltip" data-placement="top" readonly>
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-success btn-sm" id="add_parent_kaf_name"
+                                        type="button">Выбрать</button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle">Индекс<span style="color: red">*</span></td>
+                        <td>
+                            <input type="text" id="index" maxlength="10" class="form-control form-control-sm"
+                                data-toggle="popover" data-placement="right" data-content="Нужно заполнить!">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle">Название<span style="color: red">*</span></td>
+                        <td>
+                            <input type="text" id="discipline_name" maxlength="100" class="form-control form-control-sm"
+                                data-toggle="popover" data-placement="right" data-content="Нужно заполнить!">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle">Модуль<span style="color: red">*</span></td>
+                        <td>
+                            <select class="form-control form-control-sm" id="mod_val" data-toggle="popover"
+                                data-placement="right" data-content="Нужно выбрать!">
+                                <option selected value="null"></option>
+                                <?php 
+									require_once ($_SERVER['DOCUMENT_ROOT']."/back/base.php");
+									options_present("SELECT module_id
+														  , `name`
+													 FROM modules  
+													 ORDER BY module_id");
+								?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle">Относится к<span style="color: red">*</span></td>
+                        <td>
+                            <select class="form-control form-control-sm" id="part_val" data-toggle="popover"
+                                data-placement="right" data-content="Нужно выбрать!">
+                                <option selected value="null"></option>
+                                <?php 
+									require_once ($_SERVER['DOCUMENT_ROOT']."/back/base.php");
+									options_present("SELECT part_id
+														  , CONCAT(`name`,' части')
+													 FROM parts  
+													 ORDER BY part_id");
+								?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle">Нагрузка, ч<span style="color: red">*</span></td>
+                        <td>
+                            <input type="number" id="discipline_time" max="500" class="form-control form-control-sm"
+                                data-toggle="popover" data-placement="right" data-content="Нужно заполнить!">
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <?php if (isset($_GET["kafid"])) { ?>
+                <button type="button" class="btn btn-sm btn-danger" id="delete_discipline">Удалить</button>
+                <button type="button" class="btn btn-sm btn-primary" id="save_discipline">Сохранить</button>
+                <?php } else { ?>
+                <button type="button" class="btn btn-sm btn-primary" id="add_new_discipline">Создать</button>
+                <?php }; ?>
+                <button type="button" class="btn btn-sm btn-secondary close_form" data-dismiss="modal">Закрыть</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="add_parent_kaf_name_form" tabindex="-2" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title">Кафедра</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-borderless table-sm">
+                    <tr>
+                        <td class="align-middle">Институт<span style="color: red">*</span></td>
+                        <td>
+                            <select class="form-control form-control-sm" id="inst_val">
+                                <option selected value="null">Отсутствует</option>
+
+                                <?php 
+									require_once ($_SERVER['DOCUMENT_ROOT']."/back/base.php");
+									options_present("SELECT institute_id
+														  , `name`
+													 FROM institutes  
+													 ORDER BY institute_id");
+								?>
+
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="align-middle">Кафедра<span style="color: red">*</span></td>
+                        <td>
+                            <select class="form-control form-control-sm" id="kaf_val" disabled>
+                                <option selected value="null">Отсутствует</option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-success" id="save_parent_kaf_name_button">Сохранить</button>
+                <button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">Закрыть</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+$("#pul_name").mouseenter(function() {
+    $('#pul_name').removeClass('error-pointer');
+    $('#pul_name').popover('hide');
+});
+
+$("#parent_inst_name").mouseenter(function() {
+    $('#parent_inst').removeClass('error-pointer');
+    $('#parent_inst').popover('hide');
+});
+
+$("#parent_inst_name").mouseenter(function() {
+    $('#parent_inst_name').tooltip('show');
+});
+
+$("#add_parent_kaf_name").click(function() {
+    $("#add_parent_kaf_name_form").modal('show');
+});
+
+$("#save_parent_inst_name_button").click(function() {
+    var inst_id = $("#inst_val").val();
+    var inst_text = $("#inst_val option:selected").text();
+    $("#parent_inst_name").val(inst_text);
+    $("#parent_inst_name").prop('title', inst_text);
+
+    if (inst_id !== "null") {
+        $("#add_parent_inst_name").removeClass('btn-outline-success');
+        $("#add_parent_inst_name").addClass('btn-outline-primary');
+        $("#add_parent_inst_name").text('Изменить');
+    } else {
+        $("#add_parent_inst_name").removeClass('btn-outline-primary');
+        $("#add_parent_inst_name").addClass('btn-outline-success');
+        $("#add_parent_inst_name").text('Выбрать');
+    }
+    $("#add_parent_inst_name_form").modal('hide');
+});
+
+$("#add_new_discipline").click(function() {
+    let dis_name = $("#discipline_name").val();
+    let pul_description = $("#pul_description").val();
+    let inst_id = $("#inst_val").val();
+
+    if (inst_id == "null") {
+        $('#parent_inst').addClass('error-pointer');
+        $('#parent_inst').popover('show');
+    } else if (pul_name == '') {
+        $('#pul_name').addClass('error-pointer');
+        $('#pul_name').popover('show');
+    } else {
+        $.post(
+            "/back/data/db_pulpits.php", {
+                functionname: 'create_pulpit',
+                instid: inst_id,
+                name: pul_name,
+                description: pul_description
+            },
+            function(info) {
+                if (info !== '1') {
+                    alert(info);
+                } else {
+                    $('#pulpit_form').modal('hide');
+                    alert('Дисциплина создана');
+                    location.href = 'data.php?page=disciplines';
+                }
+            }
+        );
+    }
+});
+
+$("#save_discipline").click(function() {
+    let dis_name = $("#discipline_name").val();
+
+
+    if (inst_id == "null") {
+        $('#parent_inst').addClass('error-pointer');
+        $('#parent_inst').popover('show');
+    } else if (dis_name == '') {
+        $('#discipline_name').addClass('error-pointer');
+        $('#discipline_name').popover('show');
+    } else {
+        $.post(
+            "/back/data/db_disciplines.php", {
+                functionname: 'edit_discipline',
+                id: $_GET('disid'),
+                instid: inst_id,
+                name: dis_name,
+                description: pul_description
+            },
+            function(info) {
+                if (info !== '1') {
+                    alert(info);
+                } else {
+                    $('#discipline_form').modal('hide');
+                    alert('Дисциплина обновлена');
+                    location.href = 'data.php?page=disciplines';
+                }
+            }
+        );
+    }
+});
+
+$("#delete_discipline").click(function() {
+    let discipline_name = $("#discipline_name").val();
+    let discipline_form_title = $('#discipline_form_title').text();
+
+    if (confirm('Вы действительно хотите удалить объект ' + discipline_form_title + ' "' + discipline_name +
+            '"?')) {
+        $.post(
+            "/back/data/db_disciplines.php", {
+                functionname: 'remove_discipline',
+                id: $_GET('disid')
+            },
+            function(info) {
+                if (info !== '1') {
+                    alert(info);
+                } else {
+                    $('#discipline_form').modal('hide');
+                    alert('Дисциплина удалена');
+                    location.href = 'data.php?page=disciplines';
+                }
+            }
+        );
+    }
+});
+</script>
