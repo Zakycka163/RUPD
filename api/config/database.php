c45f9a09a55e73b154cc046da81c7751d4ce9e74
Dmitry St <dmitry.st@msp360.com>
2020-09-10 17:52:44 +0300

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -6,20 +6,15 @@
         private $username = "root";
         private $password = "";
         public $link;
+        public $limit;
 
         // получаем соединение с БД 
         public function get_db_link(){
 
             $this->link = null;
 
-            try {
-                $this->link = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                                             $this->username,
-                                             $this->password);
-                $this->link->exec("set names utf8");
-            } catch(PDOException $exception){
-                echo "Connection error: " . $exception->getMessage();
-            }
+            $this->link = mysqli_connect($this->host, $this->username,$this->password, $this->db_name) or die ('Connection error');
+            mysqli_query($this->link, "SET NAMES utf8");
 
             return $this->link;
         }
@@ -32,5 +27,19 @@
             return $this->link;
         }
 
+        public function get_db_limit(){
+            $database = new Database();
+            $link = $database->get_db_link();
+
+            $sql = "SELECT MAX(`value`) FROM `constants` WHERE `key` = 'limitObj'";
+            $limit_request = mysqli_query($link, $sql);
+            $limit_arr = mysqli_fetch_array($limit_request);
+            
+            $link = $database->close_db_link();
+            $this->limit = $limit_arr[0];
+
+            return $this->limit;
+
+        }
     }
 ?>
