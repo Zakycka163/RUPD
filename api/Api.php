55e253bce89941e8101efcda194b1a26d713a17f
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-09-10 22:34:55 +0300

    Default updateAction and createAction is added to parent class

--- api/Api.php
+++ api/Api.php
@@ -197,7 +197,74 @@ abstract class Api
         return $this->response('Bad Request', 400);
     }
 
-    abstract protected function createAction();
-    abstract protected function updateAction();
+    /*
+     * Метод POST
+     * Создание новой записи
+     * http://ДОМЕН/parts + JSON
+     * 
+    {
+        "name": string
+    }
+     * 
+     * @return string
+     */
+    public function createAction()
+    {
+        $data = json_decode(file_get_contents("php://input"));
+
+        if(!empty($data->name)){
+            
+            $database = new Database();
+            $link = $database->get_db_link();
+            $sql = "INSERT INTO `".$this->table_name."` (`name`) VALUES ('".$data->name."')";
+            if (mysqli_query($link, $sql)){
+                return $this->response('Data saved', 200);
+            } else {
+                return $this->response(mysqli_error($link), 500);
+            }
+            $link = $database->close_db_link();
+        } else {
+            return $this->response("Bad Request", 400);
+        }
+    }
+
+    /*
+     * Метод PUT
+     * Обновление отдельной записи (по ее id)
+     * http://ДОМЕН/parts?id= + JSON
+     * 
+    { 
+        "name": string 
+    }
+     * 
+     * @return string
+     */
+    public function updateAction()
+    {
+        $id = $this->requestParams['id'] ?? '';
+        $data = json_decode(file_get_contents("php://input"));
+
+        if( is_numeric($id) and !empty($data->name)){
+
+            $database = new Database();
+            $link = $database->get_db_link();
+            $sql_check = "SELECT 1 FROM `".$this->table_name."` WHERE id = ".$id."";
+            $result_check = mysqli_query($link, $sql_check);
+
+            if (mysqli_num_rows($result_check) == 1){
+                
+                $sql = "UPDATE `".$this->table_name."` SET `name` = '".$data->name."' WHERE id = ".$id."";
+                if (mysqli_query($link, $sql)) {
+                    return $this->response('Object updated.', 200);
+                }
+                return $this->response(mysqli_error($link), 500);
+               
+            } 
+            return $this->response('Not Found object with id = '.$id.'', 204);
+
+            $link = $database->close_db_link();
+        }
+        return $this->response('Bad Request', 400);
+    }
     
 }
