b0c718eb9980f3c608462f4508e51f9d815c4ac8
Dmitry St <dmitry.st@msp360.com>
2020-09-10 19:24:25 +0300

    api parts is completed

--- api/tables/parts.php
+++ api/tables/parts.php
@@ -37,29 +37,43 @@ class CurrentApi extends Api
         }
     }
 
-    /**
+    /*
      * Метод PUT
      * Обновление отдельной записи (по ее id)
-     * http://ДОМЕН/users/1 + параметры запроса name, email
+     * http://ДОМЕН/parts?id= + JSON
+     * 
+    { 
+        "name": string 
+    }
+     * 
      * @return string
      */
     public function updateAction()
     {
-        $parse_url = parse_url($this->requestUri[0]);
-        $userId = $parse_url['path'] ?? null;
+        $id = $this->requestParams['id'] ?? '';
+        $data = json_decode(file_get_contents("php://input"));
 
-        $database = new Database();
-        $link = $database->get_db_link();
+        if( is_numeric($id) and !empty($data->name)){
 
-        $name = $this->requestParams['name'] ?? '';
-        $email = $this->requestParams['email'] ?? '';
+            $database = new Database();
+            $link = $database->get_db_link();
+            $sql_check = "SELECT 1 FROM `".$this->table_name."` WHERE id = ".$id."";
+            $result_check = mysqli_query($link, $sql_check);
 
-        if($name && $email){
-            #TODO
-            return $this->response('Data updated.', 200);
+            if (mysqli_num_rows($result_check) == 1){
+                
+                $sql = "UPDATE `".$this->table_name."` SET `name` = '".$data->name."' WHERE id = ".$id."";
+                if (mysqli_query($link, $sql)) {
+                    return $this->response('Object updated.', 200);
+                }
+                return $this->response(mysqli_error($link), 500);
+               
+            } 
+            return $this->response('Not Found object with id = '.$id.'', 204);
+
+            $link = $database->close_db_link();
         }
-        $link = $database->close_db_link();
-        return $this->response("Update error", 400);
+        return $this->response('Bad Request', 400);
     }
 
 }
