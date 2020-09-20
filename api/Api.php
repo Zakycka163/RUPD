be1d60b26a9c29894e9e3ee5dbf67b2bb4facafb
Zakycka163 <diman.stavinskij2011@ya.ru>
2020-09-20 22:21:10 +0300

    abstract fun json_validation($data) is added

--- api/Api.php
+++ api/Api.php
@@ -166,70 +166,57 @@ abstract class Api
         return $this->response('Bad Request', 400);
     }
 
-    /*
+    /**
      * Метод POST
      * Создание новой записи
      * http://ДОМЕН/${table_name} + JSON
-     * 
-    {
-        "name": "string"
-    }
-     * 
      * @return string
      */
     public function createAction()
     {
         $data = json_decode(file_get_contents("php://input"));  
-        if( isset($data->name) ){
-            $name = htmlspecialchars($data->name) ?? '';
+        if($this->json_validation($data)){
             $database = new Database();
-            $link = $database->get_db_link();
             $errors = $database->validate_input_data($this->table_name, $data);
             if (empty($errors)) {
-                $sql = "INSERT INTO `".$this->table_name."` (`name`) VALUES ('".$name."')";
-                if (mysqli_query($link, $sql)){
+                $result = $database->insert_data_to_table($data, $this->table_name);
+                if ($result == 'ok'){
                     return $this->response('Object created', 201);
+                } else {
+                    return $this->response($result, 500);
                 }
-                return $this->response(mysqli_error($link), 500);
             }
             return $this->response($errors, 400);
-            $link = $database->close_db_link();
         } 
         return $this->response("Bad Request", 400);
     }
 
-    /*
+    /**
      * Метод PUT
      * Обновление отдельной записи (по ее id)
      * http://ДОМЕН/${table_name}?id= + JSON
-     * 
-    { 
-        "name": "string"
-    }
-     * 
      * @return string
      */
     public function updateAction()
     {
         $data = json_decode(file_get_contents("php://input"));
-        if( isset($this->requestParams['id']) and is_numeric($this->requestParams['id']) and isset($data->name)){
+        if( isset($this->requestParams['id']) and is_numeric($this->requestParams['id']) 
+        and $this->json_validation($data)){
             $id = htmlspecialchars(trim($this->requestParams['id'] ?? ''));
-            $name = htmlspecialchars($data->name) ?? '';
             $database = new Database();
-            $link = $database->get_db_link();
             $errors = $database->validate_input_data($this->table_name, $data);
             if (empty($errors)) {
-                if ($database->exist_in_table($id, $this->table_name)){                 
-                    $sql = "UPDATE `".$this->table_name."` SET `name` = '".$name."' WHERE id = ".$id."";
-                    if (mysqli_query($link, $sql)) {
-                        return $this->response('Object updated.', 200);
-                    }
-                    return $this->response(mysqli_error($link), 500);
+                if ($database->exist_in_table($id, $this->table_name)){   
+                    $result = $database->update_data_to_table($id, $data, $this->table_name);
+                    if ($result == 'ok'){
+                        return $this->response('Object updated', 200);
+                    } else {
+                        return $this->response($result, 500);
+                    }      
                 } 
                 return $this->response('Not Found object with id = '.$id.'', 204);
             }
             return $this->response($errors, 400);
-            $link = $database->close_db_link();
         }
         return $this->response('Bad Request', 400);
     }
@@ -258,4 +245,6 @@ abstract class Api
         }
         return $this->response('Bad Request', 400);
     }
+
+    abstract function json_validation($data);
 }
