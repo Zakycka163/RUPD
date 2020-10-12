9bc4d4ceb2c7b4ef1b494fd53f69e28a5db57ba0
Dmitry St <dmitry.st@msp360.com>
2020-10-12 17:32:31 +0300

    Add rest api to User page

--- front/body/single_obj/user.php
+++ front/body/single_obj/user.php
@@ -4,82 +4,15 @@
 		<div class="h4" id="page_title" style="margin-left: 35%">Аккаунты</div>
 	</div>
 </div>
-<?php
-	connect();
-	global $link;
-	$sql = "SELECT * 
-			FROM view_users
-			WHERE account_id = ".$_GET["id"]."";
-	$result = mysqli_query($link, $sql);
-	while($row = mysqli_fetch_array($result)){
-		$second_name = $row[3];
-		$first_name = $row[4];
-		$middle_name = $row[5];
-		$login = $row[1];
-		$page_title = ' '.$login;
-		if ($row[2] == 2){
-			$admin = "checked";
-		} else {
-			$admin = "";
-		}
-		$id=$_SESSION["id"];
-		$result = mysqli_query($link, "SELECT grant_id 
-									   FROM accounts 
-									   WHERE account_id='".$id."'");
-		$grant_session = implode(mysqli_fetch_assoc($result));	
-		if ($grant_session == 2){
-			$grant_to_edit_account_name = "";
-		} else {
-			$grant_to_edit_account_name = "hidden";
-		}
-	};
-	close();
-?>
 <div class="px-4 py-3 bg-light" style="min-width: 52rem;">
-	<div class="alert alert-info" style="height: 55px"><b>Аккаунт:</b><?php echo $page_title; ?></div> 
-	<div class="alert alert-secondary" style="height: 55px; top: -1rem">		
-		<input class="btn btn-success btn-sm" type="button" id="save_teach_changes" value="Сохранить">
-	</div>
+	<div class="alert alert-info" style="height: 55px"><b>Аккаунт:</b><text id="title"></text></div> 
 	<table class="table table-borderless table-sm" style="width: 40rem">
-		<tr>
-			<td class="align-middle" style="width: 7rem">Фамилия<span style="color: red">*</span></td>
-			<td>
-				<div class="input-group input-group-sm" style="right: 15px;">
-					<input class="form-control" type="text" id="second_name" value="<?php echo $second_name; ?>" maxlength="30" data-toggle="popover" data-placement="right" data-content="Не должно быть пустым! Не должно быть меньше 2 символов!">
-				</div>
-			</td>
-			<td class="align-middle"></td>
-			<td></td>
-		</tr>
-		<tr>
-			<td class="align-middle">Имя<span style="color: red">*</span></td>
-			<td>
-				<div class="input-group input-group-sm" style="right: 15px;">
-					<input class="form-control" type="text" id="first_name" value="<?php echo $first_name; ?>" maxlength="30" data-toggle="popover" data-placement="right" data-content="Не должно быть пустым! Не должно быть меньше 2 символов!">
-				</div>
-			</td>
-			<td class="align-middle"></td>
-			<td></td>
-		</tr>
-		<tr>
-			<td class="align-middle">Отчество</td>
-			<td>
-				<div class="input-group input-group-sm" style="right: 15px;">
-					<input class="form-control" type="text" id="middle_name" value="<?php echo $middle_name; ?>" maxlength="30" data-toggle="popover" data-placement="right" data-content="Не должно быть меньше 2 символов!">
-				</div>
-			</td>
-			<td class="align-middle"></td>
-			<td></td>
-		</tr>
-		<tr class="hr_section">
-			<td colspan="4"><hr></td>
-		</tr>
 		<tr>
 			<td class="align-middle">Логин</td>
 			<td>
 				<div class="input-group input-group-sm" style="right: 15px;">
-					<input class="form-control" type="text" id="login" value="<?php echo $login; ?>" readonly>
-					<div class="input-group-append" <?php echo $grant_to_edit_account_name; ?> >
+					<input class="form-control" type="text" id="login" value="" readonly>
+					<div class="input-group-append check_grant" hidden>
 						<button class="btn btn-outline-primary" id="edit_account_login" type="button">Изменить</button>
 					</div>
 				</div>
@@ -87,7 +20,7 @@
 			<td class="align-middle" style="width: 7rem">Администратор</td>
 			<td style="width: 3rem">
 				<div class="custom-control custom-switch" style="top: 5px">
-					<input type="checkbox" class="custom-control-input" <?php echo $admin; ?> id="switch">
+					<input type="checkbox" class="custom-control-input" id="switch">
 					<label class="custom-control-label"></label>
 				</div>
 			</td>
@@ -97,7 +30,7 @@
 			<td>
 				<div class="input-group input-group-sm" style="right: 15px;">
 					<input class="form-control" type="text" id="pass" value="************" readonly>
-					<div class="input-group-append" <?php echo $grant_to_edit_account_name; ?> >
+					<div class="input-group-append check_grant" hidden>
 						<button class="btn btn-outline-primary" id="edit_account_pass" type="button">Изменить</button>
 					</div>
 				</div>
@@ -105,13 +38,78 @@
 			<td class="align-middle"></td>
 			<td></td>
 		</tr>
+		<tr class="hr_section">
+			<td colspan="4"><hr></td>
+		</tr>
+		<tr>
+			<td class="align-middle" style="width: 7rem">Фамилия<span style="color: red">*</span></td>
+			<td>
+				<div class="input-group input-group-sm" style="right: 15px;">
+					<input class="form-control" type="text" id="second_name" value="" maxlength="30" data-toggle="popover" data-placement="right" data-content="Не должно быть пустым! Не должно быть меньше 2 символов!">
+				</div>
+			</td>
+			<td class="align-middle"></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td class="align-middle">Имя<span style="color: red">*</span></td>
+			<td>
+				<div class="input-group input-group-sm" style="right: 15px;">
+					<input class="form-control" type="text" id="first_name" value="" maxlength="30" data-toggle="popover" data-placement="right" data-content="Не должно быть пустым! Не должно быть меньше 2 символов!">
+				</div>
+			</td>
+			<td class="align-middle"></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td class="align-middle">Отчество</td>
+			<td>
+				<div class="input-group input-group-sm" style="right: 15px;">
+					<input class="form-control" type="text" id="middle_name" value="" maxlength="30" data-toggle="popover" data-placement="right" data-content="Не должно быть меньше 2 символов!">
+				</div>
+			</td>
+			<td class="align-middle"></td>
+			<td></td>
+		</tr>
 	</table>
+	<div class="alert alert-secondary" style="height: 55px;">		
+		<input class="btn btn-success btn-sm" type="button" id="save_teach_changes" value="Сохранить">
+	</div>
 </div>
 <?php 
 	require_once ($_SERVER['DOCUMENT_ROOT']."/front/forms/edit_account_login.php");		
 	require_once ($_SERVER['DOCUMENT_ROOT']."/front/forms/edit_account_pass.php");		
 ?>
 <script>
+$(document).ready(function(){
+	var account = new Object();
+	var teacher = new Object();
+	$.ajax({
+		url: "/api/accounts?id=<?php echo $_GET["id"];?>", 
+		type: "GET",
+		success: function(response){
+			account = response;
+			delete account.id;
+			$("#login").val(account.login);
+			if (account.grant_id == 2) {
+				$("#switch").prop("checked", true);
+				$(".check_grant").prop("hidden", false);
+			}
+			$.ajax({
+				url: "/api/teachers?id="+account.teacher_id,
+				type: "GET",
+				success: function(response){
+					teacher = response;
+					delete teacher.id;
+					$("#second_name").val(teacher.second_name);
+					$("#first_name").val(teacher.first_name);
+					$("#middle_name").val(teacher.middle_name);
+					$("#title").text(' '+teacher.second_name+' '+ teacher.first_name+' '+teacher.middle_name);
+				}
+			});
+    	}
+	});
+	
 	$("#edit_account_login").click(function(){
 		$('#edit_account_login_form').modal('show');
 	});
@@ -121,31 +119,28 @@
 	});
 
 	$("#save_teach_changes").click(function(){
-		let second_name = $("#second_name").val();
-		let first_name = $("#first_name").val();
-		let middle_name = $("#middle_name").val();
-
-		if (second_name == '' || second_name.length < 2) {
+		if ($("#second_name").val() == '' || $("#second_name").val().length < 2) {
 			$('#second_name').addClass('error-pointer');
 			$('#second_name').popover('show');
-		} else if (first_name == '' || first_name.length < 2) {
+		} else if ($("#first_name").val() == '' || $("#first_name").val().length < 2) {
 			$('#first_name').addClass('error-pointer');
 			$('#first_name').popover('show');
-		} else if (middle_name !== '' && middle_name.length < 2) {
+		} else if ($("#middle_name").val() !== '' && $("#middle_name").val().length < 2) {
 			$('#middle_name').addClass('error-pointer');
 			$('#middle_name').popover('show');
 		} else {
-			$.post(
-			 	"/back/data/db_teachers.php", 
-				{functionname: 'edit_teach_name', acc_id: <?php echo $_GET["id"];?>
-												, second_name: second_name
-												, first_name: first_name
-												, middle_name: middle_name}, 
-				function(info){
-					alert(info);
-					location.reload();
+			teacher.second_name = $("#second_name").val();
+			teacher.middle_name = $("#middle_name").val();
+			teacher.first_name = $("#first_name").val();
+			$.ajax({
+				url: "/api/teachers?id="+account.teacher_id, 
+				type: "PUT",
+				data: JSON.stringify(teacher),
+				success: function(){
+					alert('Данные обновлены');
+					location.reload()
 				}
-			);
+			});
 		}
 	});
 
@@ -163,4 +158,5 @@
 		$('#middle_name').removeClass('error-pointer');
 		$('#middle_name').popover('hide');
 	});
+});
 </script>
