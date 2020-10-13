b6b849fbafc5a137119f16dc4669f0ed192e384b
Dmitry St <dmitry.st@msp360.com>
2020-10-13 15:16:24 +0300

    view_users is added to api

--- database/view_users.sql
+++ database/view_users.sql
@@ -8,7 +8,7 @@ SELECT    acc.id
 		, teach.second_name
 		, teach.first_name
 		, teach.middle_name
-		, teach.id
+		, acc.teacher_id
 FROM  `accounts` acc
 	, `teachers` teach 
 WHERE acc.teacher_id = teach.id);
