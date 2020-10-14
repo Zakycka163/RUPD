12b7df20c4b0f0c9430747320f2d363b76402c9e
Dmitry St <dmitry.st@msp360.com>
2020-10-14 18:44:25 +0300

    Rest api is added

--- front/body/data/prof.php
+++ front/body/data/prof.php
@@ -20,75 +20,61 @@
 				<th scope="col">Дата регистации</th>
 			</tr>
 		</thead>
-		<tbody>
-			<?php
-				connect();
-				global $link;
-				$sql = "SELECT `value` FROM `constants` WHERE `key` = 'limitObj'";
-				$result = mysqli_query($link, $sql);
-				$limit = mysqli_fetch_array($result);
-				$counter = 0;
-				$sql_count = "SELECT count(*) FROM prof_standards";
-				$sql_count_result = mysqli_query($link, $sql_count);
-				$count_obj = mysqli_fetch_array($sql_count_result);
-				$sql = "SELECT    fgos.id
-								, course.number
-								, course.name
-								, prof.id
-								, prof.code
-								, prof.name
-								, prof.number
-								, prof.date
-								, prof.reg_number
-								, prof.reg_date 
-						FROM  `prof_standards` prof
-							, `fgos` fgos
-							, `courses` course
-						WHERE prof.fgos_id = fgos.id 
-						  and fgos.course_id = course.id 
-						LIMIT ".$limit[0]."";
-				$result = mysqli_query($link, $sql);
-				while($row = mysqli_fetch_array($result)){
-					$counter++;
-					echo '<tr>'."\n".'<td>'.$counter.'</td>'."\n";
-					echo '<td><a href="?page=prof&id='.$row[3].'">'.$row[4].' '.$row[5].'</td>'."\n";
-					echo '<td><a href="?page=fgos&id='.$row[0].'">'.$row[1].' '.$row[2].'</a></td>'."\n";
-					echo '<td>'.$row[6].'</td>'."\n";
-					echo '<td>'.$row[7].'</td>'."\n";
-					echo '<td>'.$row[8].'</td>'."\n";
-					echo '<td>'.$row[9].'</td>'."\n";
-					echo '</tr>'."\n";
-				};
-				close();
-			?>
+		<tbody id="data">
+			<tr>
+				<td colspan="7" style="text-align:center">Пустой список</td>
+			</tr>
 		</tbody>
 	</table>
 	<nav>
 		<ul class="pagination pagination-sm">
-			<?php if ($count_obj < $limit){ ?>
-			
-			<?php } else { ?>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">Предыдущая</a>
-				</li>
-				<li class="page-item active">
-					<a class="page-link" href="#">1</a>
-				</li>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">2</a>
-				</li>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">3</a>
-				</li>
-				<li class="page-item disabled">
-					<a class="page-link" href="#">Следующая</a>
-				</li>
-			<?php } ?>
+			<li class="page-item disabled" id="prev_round">
+				<a class="page-link" href>Предыдущая</a>
+			</li>
+			<li class="page-item disabled" id="next_round">
+				<a class="page-link" href>Следующая</a>
+			</li>
 		</ul>
 	</nav>	
 </div>
 <?php require_once ($_SERVER['DOCUMENT_ROOT']."/front/forms/fgos.php"); ?>
+<script src="/front/js/_GET.js"></script>
+<script src="/front/js/pagination.js"></script>
 <script>
+$(document).ready(function(){
+	var total;
+	var limit;
+	var round;
+	if ($_GET("round") && parseInt($_GET("round"))){
+		round = $_GET("round");
+	} else {
+		round = 1;
+	}
+	var profs = new Object();
+	var table_body = '';
+	$.ajax({
+		url: "/api/view_profs?round="+round, 
+		type: "GET",
+		success: function(response){
+			total = response.total;
+			limit = response.limit;
+			profs = response.view_profs;
+			for (const [key, prof] of Object.entries(profs)) {
+				table_body += `<tr>
+								<td>`+((key*1)+1)+`</td>
+								<td><a href="?page=prof&id=`+prof.id+`">`+prof.name+`</td>
+								<td><a href="?page=fgos&id=`+prof.fgos_id+`">`+prof.fgos_name+`</td>
+								<td>`+prof.number+`</td>
+								<td>`+prof.date+`</td>
+								<td>`+prof.reg_number+`</td>
+								<td>`+prof.reg_date+`</td>
+							  </tr>`;
+			}
+			$("#data").html(table_body);
+
+			gen_pagination(total, limit, round);
+		}		
+	});
 	$("#create_fgos_button").click(function(){
 		$.post(
 			"/back/switch_functions.php", 
@@ -100,4 +86,5 @@
 		);
 		$('#create_fgos').modal('show');
 	});
+});
 </script>
