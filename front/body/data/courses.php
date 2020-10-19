e8a9771625d852f43b12499031a58a93dbf8bc45
Dmitry St <dmitry.st@msp360.com>
2020-10-19 17:34:42 +0300

    fixed courses

--- front/body/data/courses.php
+++ front/body/data/courses.php
@@ -19,46 +19,10 @@
 				<th scope="col">Профиль</th>
 			</tr>
 		</thead>
-		<tbody>
-			<?php
-				connect();
-				global $link;
-				$sql = "SELECT `value` FROM `constants` WHERE `key` = 'limitObj'";
-				$result = mysqli_query($link, $sql);
-				$limit = mysqli_fetch_array($result);
-				$counter = 0;
-				$sql_count = "SELECT count(*) FROM courses";
-				$sql_count_result = mysqli_query($link, $sql_count);
-				$count_obj = mysqli_fetch_array($sql_count_result);
-				$sql = "SELECT    cour.id
-								, cour.number
-								, cour.name
-								, qua.name
-						FROM  `courses` cour
-							, `qualifications` qua
-						WHERE cour.qualification_id = qua.id
-						LIMIT ".$limit[0]."";
-				$result = mysqli_query($link, $sql);
-				while($row = mysqli_fetch_array($result)){
-					$counter++;
-					echo '<tr>'."\n".'<td>'.$counter.'</td>'."\n";
-					echo '<td><a href="?page=courses&id='.$row[0].'">'.$row[1].' '.$row[2].'</a></td>'."\n";
-					echo '<td>'.$row[3].'</td>'."\n";
-					$sql2 = "SELECT   prof.id
-									, prof.name
-							FROM  `profiles` prof
-							WHERE prof.course_id = ".$row[0]."";
-					$result2 = mysqli_query($link, $sql2);
-					$counter2 = 0;
-					echo '<td>'."\n";
-					while($prof = mysqli_fetch_array($result2)){
-						$counter2++;
-						echo ($counter2).'. <a href="?page=courses?profid='.$prof[0].'">'.$prof[1].'</a><br>'."\n";
-					};	
-					echo '</td>'."\n".'</tr>'."\n";
-				};
-				close();
-			?>
+		<tbody id="data">
+			<tr>
+				<td colspan="5" style="text-align:center">Пустой список</td>
+			</tr>
 		</tbody>
 	</table>
 	<nav>
@@ -76,52 +40,73 @@
 <script src="/front/js/pagination.js"></script>
 <script>
 $(document).ready(function(){
-	var total;
-	var limit;
-	var round;
-	var rows;
-	var start;
+	var data = new Object();
 	if ($_GET("round") && parseInt($_GET("round"))){
-		round = $_GET("round");
+		data.round = $_GET("round");
 	} else {
-		round = 1;
+		data.round = 1;
 	}
-	var courses = new Object();
-	var profiles = new Object();
 	var table_body = '';
+	var courses_id = '';
 	$.ajax({
-		url: "/api/view_courses?round="+round, 
+		url: "/api/view_courses?round="+data.round, 
 		type: "GET",
-		success: function(data){
-			total = data.total;
-			limit = data.limit;
-			start = ((round-1)*limit)+1;
-			courses = data.view_courses;	
+		async: false,
+		success: function(response){
+			data.total = response.total;
+			data.limit = response.limit;
+			data.start = ((data.round-1)*data.limit)+1;
+			data.courses = response.view_courses;	
+			for (var [key, course] of Object.entries(data.courses)) {
+				courses_id += course.id + ",";
+			}
+			courses_id = courses_id.substr(0, (courses_id.length - 1));
 		}
-	}).done(function() {
-		for (var [key, course] of Object.entries(courses)) {
-			$.ajax({
-				url: "/api/profiles?filter=on&course_id="+course.id, 
-				type: "GET",
-				async: false,
-				success: function(response){
-					rows = (response.total > 0)?response.total:1;
-					profiles = response.profiles;
-				}
-			}).done(function() {
-				table_body += `<tr>
-								<td rowspan="`+rows+`">`+((key*1)+start)+`</td>
-								<td rowspan="`+rows+`"><a href="?page=institutes&insid=`+institute.id+`">`+institute.name+`</td>`;
-				for (var [ind, pulpit] of Object.entries(pulpits)) {
-					table_body += `<td>`+((ind*1)+1)+`</td>
-									<td><a href="?page=institutes&kafid=`+pulpit.id+`">`+pulpit.name+`</a></td>
-								  </tr><tr>`;
-				}
-				table_body = table_body.substr(0, (table_body.length - 4));
-			});
-		}			
-		$("#data").html(table_body);
-		gen_pagination(total, limit, round);	
 	});
+	$.ajax({
+		url: "/api/profiles?filter=on&course_id="+courses_id, 
+		type: "GET",
+		async: false,
+		success: function(response){
+			for (var [key, course] of Object.entries(data.courses)) {
+				for (var [x, profile] of Object.entries(response.profiles)) {
+					if (course.id == profile.course_id){
+						if (data.courses[key].profiles === undefined){
+							data.courses[key].profiles = [profile];
+						} else {
+							data.courses[key].profiles.push(profile);
+						}
+						if (data.courses[key].rows === undefined){
+							data.courses[key].rows = 1;
+						} else {
+							++data.courses[key].rows;
+						}
+					}
+				}
+			}		
+		}
+	});
+	for (var [x, course] of Object.entries(data.courses)) {
+		table_body += `<tr>
+						<td rowspan="`+course.rows+`">`+((x*1)+data.start)+`</td>
+						<td rowspan="`+course.rows+`"><a href="?page=courses&id=`+course.id+`">`+course.name+`</td>
+						<td rowspan="`+course.rows+`">`+course.qualification+`</td>`;
+		if (course.profiles === undefined){
+			table_body += `<td></td>
+							<td></td>
+							</tr><tr>`;
+		} else {
+			for (var [y, profile] of Object.entries(course.profiles)) {
+				table_body += `<td>`+((y*1)+1)+`</td>
+								<td><a href="?page=courses&profid=`+profile.id+`">`+profile.name+`</a></td>
+							</tr><tr>`;
+			}
+			
+		}
+		table_body = table_body.substr(0, (table_body.length - 4));
+	}	
+	$("#data").html(table_body);
+	//$("#data").text(JSON.stringify(data));
+	gen_pagination(data.total, data.limit, data.round);	
 });
 </script>
