ddd50bcc343fc713daea6a66f3a720a7a08e1d69
Dmitry St <dmitry.st@msp360.com>
2020-10-20 19:27:44 +0300

    Update otf.php

--- front/body/data/otf.php
+++ front/body/data/otf.php
@@ -5,10 +5,10 @@
 	</div>
 </div>
 <div class="px-4 py-3 bg-light" style="margin-bottom: 4rem;">
-	<div class="alert alert-info row" style="height: 55px">
-		<label for="fgos" class="col-sm-1 form-control-sm">ФГОС</label>
+	<div class="alert alert-info row" style="height: 55px;">
+		<label for="fgos" class="form-control-sm">ФГОС: </label>
 		<select class="form-control form-control-sm col-sm-3" id="fgos"></select>
-		<label for="prof" class="col-sm-2 form-control-sm">Проф стандарт</label>
+		<label for="prof" class="form-control-sm">Проф стандарт: </label>
 		<select class="form-control form-control-sm col-sm-3" id="prof" disabled></select>
 	</div>
 	<div class="alert alert-secondary col" style="height: 55px" hidden id="work_panel">
@@ -20,17 +20,20 @@
 		<thead>
 			<tr>
 				<th scope="col" style="width: 2rem">№</th>
-				<th scope="col">Профессиональный стандарт</th>
+				<th scope="col">Проф стандарт</th>
+				<th scope="col" style="width: 2rem">№</th>
 				<th scope="col">ОТФ</th>
 				<th scope="col">Уровень квалификации</th>
-				<th scope="col">Дата приказа</th>
-				<th scope="col">Номер регистации</th>
-				<th scope="col">Дата регистации</th>
+				<th scope="col" style="width: 2rem">№</th>
+				<th scope="col">ТФ</th>
+				<th scope="col">Тип активности</th>
+				<th scope="col" style="width: 2rem">№</th>
+				<th scope="col">Активность</th>
 			</tr>
 		</thead>
 		<tbody id="data">
 			<tr>
-				<td colspan="7" style="text-align:center">Пустой список</td>
+				<td colspan="10" style="text-align:center">Пустой список</td>
 			</tr>
 		</tbody>
 	</table>
@@ -56,11 +59,22 @@ $(document).ready(function(){
 		success: function(response){
 			var fgos_option = '<option value="0"></option>';
 			for (var [key, fgos] of Object.entries(response.view_fgos)) {
-				fgos_option += `<option value="`+fgos.id+`">`+fgos.name+`</option>`;
+				var state = '';
+				if ($_GET("fgos_id") && $_GET("fgos_id") == fgos.id){
+					state = 'selected';
+				}
+				fgos_option += `<option value="`+fgos.id+`"`+state+`>`+fgos.name+`</option>`;
 			}
 			$("#fgos").html(fgos_option);
 		}
 	});
+	if ($_GET("fgos_id") && parseInt($_GET("fgos_id"))){
+		$("#fgos").trigger("change");
+		if ($_GET("prof_id") && parseInt($_GET("prof_id"))){
+			$("#prof").trigger("change");
+		}
+	}
+	
 	
 	/*if ($_GET("round") && parseInt($_GET("round"))){
 		data.round = $_GET("round");
@@ -131,11 +145,15 @@ $(document).ready(function(){
 });
 
 $("#fgos").change(function() {
-	$('#data').html('<tr><td colspan="7" style="text-align:center">Пустой список</td></tr>');
+	$('#data').html('<tr><td colspan="10" style="text-align:center">Пустой список</td></tr>');
 	$('#prof').html('<option value="0"></option>');
 	$('#work_panel').prop('hidden', true);
-	var fgos_id = $("#fgos").val();
-	if (fgos_id == 0) {
+	var fgos_id = '';
+	if ($_GET("fgos_id") && parseInt($_GET("fgos_id"))) {
+		fgos_id = $_GET("fgos_id");
+	} else { fgos_id = $("#fgos").val(); }
+
+	if (fgos_id == 0 || fgos_id === null) {
 		$('#prof').prop('disabled', true);
 	} else {
 		$.ajax({
@@ -145,7 +163,11 @@ $("#fgos").change(function() {
 				var prof_option = '<option value="0"></option>';
 				if (response.view_profs !== undefined){
 					for (var [key, prof] of Object.entries(response.view_profs)) {
-						prof_option += `<option value="`+prof.id+`">`+prof.name+`</option>`;
+						var state = '';
+						if ($_GET("prof_id") && $_GET("prof_id") == prof.id){
+							state = 'selected';
+						}
+						prof_option += `<option value="`+prof.id+`"`+state+`>`+prof.name+`</option>`;
 					}
 				}
 				$("#prof").html(prof_option);
@@ -157,25 +179,43 @@ $("#fgos").change(function() {
 
 $("#prof").change(function() {
 	$('#work_panel').prop('hidden', true);
-	var prof_id = $("#prof").val();
-	var fgos_id = $("#fgos").val();
-	if (prof_id == 0) {
-		$('#data').html('<tr><td colspan="7" style="text-align:center">Пустой список</td></tr>');
+	var prof_id = '';
+	var fgos_id = '';
+	if ($_GET("fgos_id") && parseInt($_GET("fgos_id"))) {
+		fgos_id = $_GET("fgos_id");
+	} else { fgos_id = $("#fgos").val(); }
+	
+	if ($_GET("prof_id") && parseInt($_GET("prof_id"))) {
+		prof_id = $_GET("prof_id");
+	} else { prof_id = $("#prof").val(); }
+
+	if (prof_id == 0 || prof_id === null) {
+		$('#data').html('<tr><td colspan="10" style="text-align:center">Пустой список</td></tr>');
 	} else {
+		var data = new Object();
+		var table_body = '';
+		var tfs_id = '';
 		$.ajax({
-			url: "/api/view_profs?filter=on&fgos_id="+fgos_id, 
+			url: "/api/view_tfuns?filter=on&prof_id="+prof_id, 
 			type: "GET",
+			async: false,
 			success: function(response){
-				var prof_option = '<option value="0"></option>';
-				if (response.view_profs !== undefined){
-					for (var [key, prof] of Object.entries(response.view_profs)) {
-						prof_option += `<option value="`+prof.id+`">`+prof.name+`</option>`;
+				data.total = response.total;
+				data.limit = response.limit;
+				data.start = ((data.round-1)*data.limit)+1;
+				data.tfuns = response.view_tfuns;
+				for (var [key, tfun] of Object.entries(data.tfuns)) {
+					if (tfun.tf_id !== null && !tfs_id.includes(tfun.tf_id)){
+						tfs_id += tfun.tf_id + ",";
 					}
 				}
-				$("#prof").html(prof_option);
-				$('#prof').prop('disabled', false);
+				tfs_id = tfs_id.substr(0, (tfs_id.length - 1));
 			}
 		});
+		//$("#data").html(table_body);
+		$('#work_panel').prop('hidden', false);
+		$("#data").text(tfs_id);
+		gen_pagination(data.total, data.limit, data.round);
 	}
 });
 </script>
