332cea8f675fc8b77a06a721950e102a941d7651
Dmitry St <dmitry.st@msp360.com>
2020-10-20 13:11:25 +0300

    view_tfuns is added

--- database/view_tfuns.sql
+++ database/view_tfuns.sql
@@ -2,12 +2,11 @@ CREATE
 /* [DEFINER = { user | CURRENT_USER }]*/
 VIEW view_tfuns
 AS (
-SELECT  prof.id
-      , prof.name
-	  , prof.fgos_id
-	  , prof.fgos_name
-FROM  `view_profs` prof
-	, `general_work_functions` otf
-    , `work_functions` tf
-    , `activities` act
-WHERE	prof.id = otf.);
+SELECT  otf.prof_standard_id as prof_id
+	  , otf.id
+      , CONCAT_WS('. ',otf.code,otf.name) as name
+      , otf.level
+      , tf.id as tf_id
+      , CONCAT_WS('. ',tf.code,tf.name) as tf_name
+FROM  `general_work_functions` otf
+LEFT JOIN `work_functions` tf ON otf.id = tf.general_work_function_id);
